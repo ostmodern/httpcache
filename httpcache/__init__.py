09f6e5b7acfafbf9ee77ad5acd1d1aa018bd1d66
Jonas Ghyllebert <jonas.ghyllebert@ostmodern.co.uk>
2016-08-19 16:17:55 +0100

    Bump version

--- httpcache/__init__.py
+++ httpcache/__init__.py
@@ -6,7 +6,7 @@ __init__.py
 Defines the public API to the httpcache module.
 """
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from .cache import HTTPCache
 from .adapter import CachingHTTPAdapter
